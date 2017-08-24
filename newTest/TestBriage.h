1805b0304790c02c1329b7354e2ac723757765ad
weichao <502885507@qq.com>
2017-08-24 15:39:44 +0800

    这是测试

---
+++ newTest/TestBriage.h
@@ -0,0 +1,13 @@
+//
+//  TestBriage.h
+//  newTest
+//
+//  Created by dream1024 on 16/7/26.
+//  Copyright © 2016年 dream1024. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestBriage : UIViewController
+
+@end
---
+++ newTest/TestBriage.m
@@ -0,0 +1,38 @@
+//
+//  TestBriage.m
+//  newTest
+//
+//  Created by dream1024 on 16/7/26.
+//  Copyright © 2016年 dream1024. All rights reserved.
+//
+
+#import "TestBriage.h"
+
+@interface TestBriage ()
+
+@end
+
+@implementation TestBriage
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
